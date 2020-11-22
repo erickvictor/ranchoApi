fa756396f8818147601bd4632cf930e9a27ab1bf
Erick Bernardo <erickvictor@gmail.com>
2020-11-22 14:13:07 -0400

    Create POST and PUT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express"),
+  morgan = require("morgan"),
   app = express(),
   mysql = require("mysql"), // import mysql module
   cors = require("cors"),
@@ -9,7 +10,8 @@ db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "rancho",
+  database: "rancho_test",
+  multipleStatements: true,
 });
 
 // make server object that contain port property and the value for our server.
@@ -17,12 +19,26 @@ var server = {
   port: 4050,
 };
 
+app.use(morgan("tiny"));
+
 // routers
 const usersRouter = require("./routes/militars");
 const arranchadosRouter = require("./routes/arranchados");
 // use the modules
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
 // use router
 app.use("/militars", usersRouter);
 app.use("/arranchados", arranchadosRouter);
--- routes/arranchados.js
+++ routes/arranchados.js
@@ -23,15 +23,61 @@ router.get("/list/:date", function (req, res) {
     FROM
       arranchamento
     WHERE
-      data ='${date}') AS newRancho ON M.cpf = newRancho.cpf`;
-  console.log(date);
-  console.log(sql);
+      data ='${date}') AS newRancho ON M.cpf = newRancho.cpf
+    ORDER BY
+      arranchamentoId desc,
+      M.hierarquia`;
+  // console.log(date);
+  // console.log(sql);
   db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.json({
       status: 200,
       data,
       message: "Arranchados lists retrieved successfully",
+    });
+  });
+});
+
+// create new user
+router.post("/new", function (req, res) {
+  let sql = `INSERT INTO arranchamento(cpf, data, cafe, almoco, jantar, hierarquia) VALUES (?)`;
+  let values = [
+    req.body.cpf,
+    req.body.data,
+    req.body.cafe,
+    req.body.almoco,
+    req.body.jantar,
+    req.body.hierarquia,
+  ];
+  console.log(values);
+  db.query(sql, [values], function (err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 201,
+      message: "New user added successfully",
+    });
+  });
+});
+
+// update new user
+router.put("/:id", function (req, res) {
+  const id = req.params.id;
+  console.log(req.body);
+  let sql = `UPDATE arranchamento SET ? WHERE id=${id}`;
+  let values = {
+    cpf: req.body.cpf,
+    data: req.body.data,
+    cafe: req.body.cafe,
+    almoco: req.body.almoco,
+    jantar: req.body.jantar,
+    hierarquia: req.body.hierarquia,
+  };
+  db.query(sql, [values], function (err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 201,
+      message: "Update user added successfully",
     });
   });
 });
